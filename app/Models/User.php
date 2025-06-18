67eb04cdba1f8e8bf86fab312b443e6174c5e9f3
abawan7 <mlkabawan336@gmail.com>
2025-06-18 18:04:13 +0500

    Changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;  // Add this import
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject  // Implement the JWTSubject interface
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
